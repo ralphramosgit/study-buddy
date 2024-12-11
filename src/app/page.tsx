3d7cd217c2816d1287d2a769b2d86831384b9c31
Ralph Jhon Ramos <22ralph.ramos@gmail.com>
2024-12-10 22:35:08 -1000

    cleaned up homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Col, Container, Row, Button } from 'react-bootstrap';
+import { Col, Container, Row, Button, Image } from 'react-bootstrap';
 import './globals.css';
 import { useSession } from 'next-auth/react';
 
@@ -19,7 +19,7 @@ const Home = () => {
   return (
     <main>
       <Container id="landing-page" fluid className="py-3">
-        <Row id="home" className="about-row align-middle text-center vh-100">
+        <Row id="home" className=" align-middle text-center vh-100">
           <Col xs={12} className="d-flex flex-column justify-content-center align-items-center">
             <h1 className="title">
               <strong>Study</strong>
@@ -27,7 +27,7 @@ const Home = () => {
             <h1 className="title">
               <strong>Buddy</strong>
             </h1>
-            <h2 className="py-4">&quot;Your study success starts with a buddy&quot;</h2>
+            <h2 className="quote py-4">&quot;Your study success starts with a buddy&quot;</h2>
             <Button className="get-started" onClick={handleClick}>
               <span className="text-decoration-none" style={{ color: 'black' }}>
                 Get Started
@@ -35,29 +35,69 @@ const Home = () => {
             </Button>
           </Col>
         </Row>
-        <Row id="about" className="tutorial-row vh-100">
-          <h1 className="mt-5">What is Study Buddy?</h1>
-          <Col className="col-centered">
-            <div className="container_temp">
-              <div className="overlay_temp text-center">Project Image Here</div>
-            </div>
+        {/* <Row id="about" className="about-row vh-100">
+          <h1 className="py-5 px-5">
+            <strong>What is Study Buddy?</strong>
+          </h1>
+          <div className="container">
+            <div className="overlay" />
+            <p className="text">
+              Study buddy is an application to find a buddy to study with. With study buddy, you can find and schedule
+              study sessions.
+            </p>
+          </div>
+        </Row> */}
+
+        <Row id="about" className="about-row vh-100">
+          <h1 className="mt-5 px-5">
+            <strong>What Is Study Buddy?</strong>
+          </h1>
+          <Col className="about-col-centered pt-4">
+            <Col className="about-col-centered px-5">
+              <Image src="/owlbuddy.jpg" alt="Owl Buddy" className="aboutImg" />
+            </Col>
+            <Col className="about-col-centered px-5 ">
+              <div className="aboutDes pt-4">
+                <h3 className="aboutP">
+                  Study buddy is an application to find a buddy to study with. With study buddy, you can find and
+                  schedule study sessions. You can add specific details about your sessions such as date, time, class,
+                  and place. Also feel free to find and add study playlists. Enjoy.
+                </h3>
+              </div>
+            </Col>
           </Col>
-          <Col className="col-centered">
-            <div className="container">
-              <div className="overlay" />
-              <p className="text">
-                Study buddy is an application to find a buddy to study with. With study buddy, you can find and schedule
-                study sessions.
-              </p>
-            </div>
-          </Col>
+
+          {/* <Row>
+            <Col className="about-col">
+              <img src="/images/landing-page.png" alt="landing page" className="about-img" />
+            </Col>
+            <Col>
+              <div className="about">
+                <p>
+                  Study buddy is an application to find a buddy to study with. With study buddy, you can find and
+                  schedule study sessions.
+                </p>
+              </div>
+            </Col>
+          </Row> */}
         </Row>
         <Row id="tutorial" className=" vh-100">
-          <h1 className="mt-5">Tutorial</h1>
+          <h1 className="mt-5 px-5">
+            <strong>Tutorial</strong>
+          </h1>
           <Col className="vid-col-centered">
-            <div className="container">
-              <div className="vid-overlay" />
-              <p className="text pt-5">This is the video demo</p>
+            <div className="tutorialDiv pt-4">
+              <iframe
+                width="1100"
+                height="618.75"
+                src="https://www.youtube.com/embed/Tn6-PIqc4UM?si=c9XLqVadHaFoUsFh"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write;
+                encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              />
             </div>
           </Col>
         </Row>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,15 +16,9 @@ const NavBar: React.FC = () => {
     <Navbar className="custom-navbar" expand="lg">
       <Container>
         <div className="navbar-div">
-        <Navbar.Brand href="/">
-          <Image
-            src="/Logo.PNG"
-            width="70"
-            height="70"
-            className="logo d-inline-block align-top"
-            alt="logo"
-          />
-        </Navbar.Brand>
+          <Navbar.Brand href="/">
+            <Image src="/Logo.PNG" width="70" className="logo d-inline-block align-top" alt="logo" />
+          </Navbar.Brand>
         </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -62,35 +56,35 @@ const NavBar: React.FC = () => {
               : ''}
           </Nav>
           <div className="navbar-div">
-          <Nav className="justify-content-end">
-            {session ? (
-              <NavDropdown id="login-dropdown" title={currentUser}>
-                <NavDropdown.Item id="login-dropdown-sign-out" href="/myProfile">
-                  <PersonFill className="mx-2" />
-                  My Profile
-                </NavDropdown.Item>
-                <NavDropdown.Item id="login-dropdown-sign-out" href="/api/auth/signout">
-                  <BoxArrowRight className="mx-2" />
-                  Sign Out
-                </NavDropdown.Item>
-                <NavDropdown.Item id="login-dropdown-change-password" href="/auth/change-password">
-                  <Lock className="mx-2" />
-                  Change Password
-                </NavDropdown.Item>
-              </NavDropdown>
-            ) : (
-              <NavDropdown id="login-dropdown" title="Login">
-                <NavDropdown.Item id="login-dropdown-sign-in" href="/auth/signin">
-                  <PersonFill />
-                  Sign in
-                </NavDropdown.Item>
-                <NavDropdown.Item id="login-dropdown-sign-up" href="/auth/signup">
-                  <PersonPlusFill />
-                  Sign up
-                </NavDropdown.Item>
-              </NavDropdown>
-            )}
-          </Nav>
+            <Nav className="justify-content-end">
+              {session ? (
+                <NavDropdown id="login-dropdown" title={currentUser}>
+                  <NavDropdown.Item id="login-dropdown-sign-out" href="/myProfile">
+                    <PersonFill className="mx-2" />
+                    My Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item id="login-dropdown-sign-out" href="/api/auth/signout">
+                    <BoxArrowRight className="mx-2" />
+                    Sign Out
+                  </NavDropdown.Item>
+                  <NavDropdown.Item id="login-dropdown-change-password" href="/auth/change-password">
+                    <Lock className="mx-2" />
+                    Change Password
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <NavDropdown id="login-dropdown" title="Login">
+                  <NavDropdown.Item id="login-dropdown-sign-in" href="/auth/signin">
+                    <PersonFill />
+                    Sign in
+                  </NavDropdown.Item>
+                  <NavDropdown.Item id="login-dropdown-sign-up" href="/auth/signup">
+                    <PersonPlusFill />
+                    Sign up
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
+            </Nav>
           </div>
         </Navbar.Collapse>
       </Container>
